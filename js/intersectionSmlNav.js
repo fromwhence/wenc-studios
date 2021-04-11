5b25aaa0fe1741970d385303008666fe3c3c0340
fromwhence <wenc.matt@gmail.com>
2021-04-11 16:06:45 -0500

    Added new observer for mobile and tablet nav

---
+++ js/intersectionSmlNav.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function selectElementByClass(className) {
+  return document.querySelector(`.${className}`);
+}
+
+const sectionsSmall = [
+  selectElementByClass('about'),
+  selectElementByClass('services'),
+  selectElementByClass('rates'),
+  selectElementByClass('schedule'),
+  selectElementByClass('contact'),
+];
+
+const navItemsSmall = {
+  about: selectElementByClass('aboutNavItemSmall'),
+  services: selectElementByClass('servicesNavItemSmall'),
+  rates: selectElementByClass('ratesNavItemSmall'),
+  schedule: selectElementByClass('scheduleNavItemSmall'),
+  contact: selectElementByClass('contactNavItemSmall'),
+};
+
+let thresholdValue = window.innerWidth < 768 ? 0 : 0.25;
+console.log(thresholdValue);
+// intersection observer setup
+const observerOptionsSmall = {
+  root: null,
+  rootMargin: '0px',
+  threshold: thresholdValue,
+};
+
+function observerCallbackSmall(entries, observerSmall) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // get the nav item corresponding to the id of the section
+      // that is currently in view
+      const navItemSmall = navItemsSmall[entry.target.id];
+      // add 'active' class on the navItem
+      navItemSmall.classList.add('active');
+      // remove 'active' class from any navItem that is not
+      // same as 'navItem' defined above
+      Object.values(navItemsSmall).forEach(item => {
+        if (item != navItemSmall) {
+          item.classList.remove('active');
+        }
+      });
+    }
+  });
+}
+
+const observerSmall = new IntersectionObserver(
+  observerCallbackSmall,
+  observerOptionsSmall
+);
+
+sectionsSmall.forEach(sec => observerSmall.observe(sec));
